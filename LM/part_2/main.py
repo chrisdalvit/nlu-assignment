d45593d8acc81835f0be24d8a0ae49ec9080c06f
Christian Dalvit <chris.dalvit@gmail.com>
2024-09-26 17:53:17 +0200

    add model for part 2

--- LM/part_2/main.py
+++ LM/part_2/main.py
@@ -1,10 +1,71 @@
+import argparse
+import copy
+
+import torch
+import torch.optim as optim
+import numpy as np
+
+from model import LM_RNN
+from utils import Environment, Logger
+from functions import train_loop, eval_loop
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--optim", default="sgd")
+# Experiment also with a smaller or bigger model by changing hid and emb sizes
+# A large model tends to overfit
+parser.add_argument("--emb-size", type=int, default=300)
+# With SGD try with an higher learning rate (> 1 for instance)
+parser.add_argument("--lr", type=float, default=1.0)
+# Clip the gradient
+parser.add_argument("--clip", type=int, default=5)
+# Don't forget to experiment with a lower training batch size
+# Increasing the back propagation steps can be seen as a regularization step
+parser.add_argument("--train-batch-size", type=int, default=64)
+parser.add_argument("--dev-batch-size", type=int, default=128)
+parser.add_argument("--test-batch-size", type=int, default=128)
+parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--out-dropout", type=float, default=0) 
+parser.add_argument("--emb-dropout", type=float, default=0)
+
+def run_epochs(model, optimizer, criterion_train, criterion_eval, env, logger, patience=3):
+    best_ppl = float('inf')
+    best_model = None
+    current_patience = patience
+    for epoch in range(env.args.epochs):
+        loss_train = train_loop(env.dataloaders["train"], optimizer, criterion_train, model, env.args.clip)
+        print(loss_train)
+        ppl_dev, loss_dev = eval_loop(env.dataloaders["dev"], criterion_eval, model)
+        if  ppl_dev < best_ppl: # the lower, the better
+            best_ppl = ppl_dev
+            best_model = copy.deepcopy(model).to('cpu')
+            current_patience = patience
+        else:
+            current_patience -= 1
+        logger.add_epoch_log(epoch, np.asarray(loss_train).mean(), np.asarray(loss_dev).mean(), ppl_dev)
+        if current_patience <= 0:
+            break
+        
+    best_model.to(env.device)
+    return best_model
+
 def main():
+    args = parser.parse_args()
+    env = Environment(args)
+    logger = Logger(env)
     model = LM_RNN(
             "lstm",
             env.args.emb_size, 
-            env.args.hid_size, 
-            len(env.lang)
+            len(env.lang),
+            weight_tying=True,
+            variational_dropout=True
         ).to(env.device)
-
+    optimizer = optim.SGD(model.parameters(), lr=env.args.lr)
+    criterion_train = torch.nn.CrossEntropyLoss(ignore_index=env.pad_token_id)
+    criterion_eval = torch.nn.CrossEntropyLoss(ignore_index=env.pad_token_id, reduction='sum')
+    best_model = run_epochs(model, optimizer, criterion_train, criterion_eval, env, logger)
+    final_ppl, _ = eval_loop(env.dataloaders["test"], criterion_eval, best_model)
+    logger.set_final_ppl(final_ppl)
+    print(logger.dumps())
+    
 if __name__ == "__main__":
     main()
--- LM/part_2/model.py
+++ LM/part_2/model.py
@@ -1,24 +1,61 @@
+import torch
 import torch.nn as nn
+
+class VariationalDropout(nn.Module):
+    
+    def __init__(self, p):
+        super(VariationalDropout, self).__init__()
+        self.p = p
+        
+    def forward(self, x):
+        if not self.training:
+            return x
+        mask = torch.ones(x.size(1), x.size(2)).bernoulli(1-self.p)
+        mask = mask.expand_as(x)
+        return (mask * x) / (1 - self.p)
+        
+        
 
 class LM_RNN(nn.Module):
     def __init__(
         self, 
+        rec_layer,
         emb_size, 
-        hidden_size, 
         output_size, 
         out_dropout=0.0,
         emb_dropout=0.0,
         pad_index=0, 
         n_layers=1,
-        weight_tying=False
+        weight_tying=False,
+        variational_dropout=False
     ):
         super(LM_RNN, self).__init__()
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
-        self.emb_dropout = nn.Dropout(p=emb_dropout) if emb_dropout > 0.0 else None
-        self.rnn = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)
+        
+        if variational_dropout and emb_dropout > 0.0:
+            self.emb_dropout = VariationalDropout(p=emb_dropout)
+        elif emb_dropout > 0.0:
+            self.emb_dropout = nn.Dropout(p=emb_dropout)
+        else:
+            self.emb_dropout = None
+            
+        if rec_layer == "rnn":
+            self.rnn = nn.RNN(emb_size, emb_size, n_layers, bidirectional=False, batch_first=True)
+        elif rec_layer == "lstm":
+            self.rnn = nn.LSTM(emb_size, emb_size, n_layers, bidirectional=False, batch_first=True)
+        else:
+            self.rnn = None
+        
         self.pad_token = pad_index
-        self.out_dropout = nn.Dropout(p=out_dropout) if out_dropout > 0.0 else None
-        self.output = nn.Linear(hidden_size, output_size)
+        
+        if variational_dropout and out_dropout > 0.0:
+            self.out_dropout = VariationalDropout(p=out_dropout)
+        elif out_dropout > 0.0:
+            self.out_dropout = nn.Dropout(p=out_dropout)
+        else:
+            self.out_dropout = None
+        
+        self.output = nn.Linear(emb_size, output_size)
         if weight_tying:
             self.output.weight = self.embedding.weight
             
