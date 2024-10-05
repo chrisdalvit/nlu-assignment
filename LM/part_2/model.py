12bb669556d4e70ea64a82557985f375c3b8abd6
Dalvit <christian.dalvit@\unitn.it>
2024-10-05 11:22:02 +0200

    add hidden dropout

--- LM/part_2/model.py
+++ LM/part_2/model.py
@@ -24,6 +24,7 @@ class LM_RNN(nn.Module):
         output_size, 
         out_dropout=0.0,
         emb_dropout=0.0,
+        hid_dropout=0.0,
         pad_index=0, 
         n_layers=1,
         weight_tying=False,
@@ -39,14 +40,20 @@ class LM_RNN(nn.Module):
         else:
             self.emb_dropout = None
             
-        if rec_layer == "rnn":
-            self.rnn = nn.RNN(emb_size, emb_size, n_layers, bidirectional=False, batch_first=True)
-        elif rec_layer == "lstm":
-            self.rnn = nn.LSTM(emb_size, emb_size, n_layers, bidirectional=False, batch_first=True)
+        if variational_dropout and hid_dropout > 0.0:
+            self.hid_dropout = VariationalDropout(p=hid_dropout)
+        elif hid_dropout > 0.0:
+            self.hid_dropout = nn.Dropout(p=hid_dropout)
         else:
-            self.rnn = None
-        
-        self.pad_token = pad_index
+            self.hid_dropout = None
+           
+        if rec_layer == "rnn":
+            stacked_rnn = [ nn.RNN(emb_size, emb_size, batch_first=True) for _ in range(n_layers) ]
+        elif rec_layer == "lstm":
+            stacked_rnn = [ nn.LSTM(emb_size, emb_size, batch_first=True) for _ in range(n_layers) ]
+        else:
+            stacked_rnn = None
+        self.rnns = nn.ModuleList(stacked_rnn)
         
         if variational_dropout and out_dropout > 0.0:
             self.out_dropout = VariationalDropout(p=out_dropout)
@@ -64,7 +71,15 @@ class LM_RNN(nn.Module):
         emb = self.embedding(input_sequence)
         if self.emb_dropout:
             emb = self.emb_dropout(emb)
-        rnn_out, _  = self.rnn(emb)
+        
+        n_layers = len(self.rnns)
+        rnn_out = emb
+        for idx, rnn in enumerate(self.rnns):
+            rnn.flatten_parameters() # for compact memory usage
+            rnn_out, _  = rnn(rnn_out)
+            if self.hid_dropout and idx < n_layers:
+                rnn_out = self.hid_dropout(rnn_out)
+        
         if self.out_dropout:
             rnn_out = self.out_dropout(rnn_out)
         output = self.output(rnn_out).permute(0,2,1)
