276162a213c5a8ea441e945a355944560a256dbb
Dalvit <christian.dalvit@\unitn.it>
2024-10-05 11:22:48 +0200

    rewrite train loop

--- LM/part_2/functions.py
+++ LM/part_2/functions.py
@@ -8,21 +8,15 @@ def train_loop(data, optimizer, criterion, model, clip=5):
     model.train()
     loss_array = []
     number_of_tokens = []
-
     for sample in data:
-        optimizer.zero_grad() # Zeroing the gradient
+        optimizer.zero_grad()
         output = model(sample['source'])
         loss = criterion(output, sample['target'])
         loss_array.append(loss.item() * sample["number_tokens"])
         number_of_tokens.append(sample["number_tokens"])
-        loss.backward() # Compute the gradient, deleting the computational graph
-        # clip the gradient to avoid explosioning gradients
+        loss.backward() 
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
-        optimizer.step() # Update the weights
-
-    if isinstance(optimizer, NTAvgSGD):
-        optimizer.update_weights()
-    
+        optimizer.step()
     return sum(loss_array)/sum(number_of_tokens)
 
 def eval_loop(data, eval_criterion, model):
@@ -31,13 +25,12 @@ def eval_loop(data, eval_criterion, model):
     loss_array = []
     number_of_tokens = []
     # softmax = nn.Softmax(dim=1) # Use Softmax if you need the actual probability
-    with torch.no_grad(): # It used to avoid the creation of computational graph
+    with torch.no_grad():
         for sample in data:
             output = model(sample['source'])
             loss = eval_criterion(output, sample['target'])
             loss_array.append(loss.item())
             number_of_tokens.append(sample["number_tokens"])
-
     ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
     loss_to_return = sum(loss_array) / sum(number_of_tokens)
     return ppl, loss_to_return
--- LM/part_2/main.py
+++ LM/part_2/main.py
@@ -8,16 +8,14 @@ import numpy as np
 
 from model import LM_RNN
 from utils import Environment, Logger, NTAvgSGD
-from functions import train_loop, eval_loop
+from functions import train_loop, eval_loop, init_weights
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--optim", default="sgd")
 # Experiment also with a smaller or bigger model by changing hid and emb sizes
 # A large model tends to overfit
 parser.add_argument("--emb-size", type=int, default=300)
-# With SGD try with an higher learning rate (> 1 for instance)
 parser.add_argument("--lr", type=float, default=1.0)
-# Clip the gradient
 parser.add_argument("--clip", type=int, default=5)
 # Don't forget to experiment with a lower training batch size
 # Increasing the back propagation steps can be seen as a regularization step
@@ -27,27 +25,10 @@ parser.add_argument("--test-batch-size", type=int, default=128)
 parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--out-dropout", type=float, default=0) 
 parser.add_argument("--emb-dropout", type=float, default=0)
-
-def run_epochs(model, optimizer, criterion_train, criterion_eval, env, logger, patience=3):
-    best_ppl = float('inf')
-    best_model = None
-    current_patience = patience
-    for epoch in range(env.args.epochs):
-        loss_train = train_loop(env.dataloaders["train"], optimizer, criterion_train, model, env.args.clip)
-        print(loss_train)
-        ppl_dev, loss_dev = eval_loop(env.dataloaders["dev"], criterion_eval, model)
-        if  ppl_dev < best_ppl: # the lower, the better
-            best_ppl = ppl_dev
-            best_model = copy.deepcopy(model).to('cpu')
-            current_patience = patience
-        else:
-            current_patience -= 1
-        logger.add_epoch_log(epoch, np.asarray(loss_train).mean(), np.asarray(loss_dev).mean(), ppl_dev)
-        if current_patience <= 0:
-            break
-        
-    best_model.to(env.device)
-    return best_model
+parser.add_argument("--hid-dropout", type=float, default=0)
+parser.add_argument("--weight-tying", action='store_true')
+parser.add_argument("--variational-dropout", action='store_true')
+parser.add_argument("--num-layers", type=int, default=1)
 
 def main():
     args = parser.parse_args()
@@ -57,10 +38,15 @@ def main():
             "lstm",
             env.args.emb_size, 
             len(env.lang),
-            weight_tying=True,
-            variational_dropout=True
+            emb_dropout=env.args.emb_dropout,
+            out_dropout=env.args.out_dropout,
+            hid_dropout=env.args.hid_dropout,
+            weight_tying=env.args.weight_tying,
+            variational_dropout=env.args.variational_dropout,
+            n_layers=env.args.num_layers,
+            pad_index=env.pad_token_id
         ).to(env.device)
-    
+    model.apply(init_weights)
     criterion_train = torch.nn.CrossEntropyLoss(ignore_index=env.pad_token_id)
     criterion_eval = torch.nn.CrossEntropyLoss(ignore_index=env.pad_token_id, reduction='sum')
     optimizer = None
@@ -75,8 +61,28 @@ def main():
             L = ceil(len(env.dataloaders["train"]) / env.args.train_batch_size),
             n = 5
         )
-        
-    best_model = run_epochs(model, optimizer, criterion_train, criterion_eval, env, logger)
+    best_ppl = float('inf')
+    best_model = None
+    current_patience = 3
+    for epoch in range(env.args.epochs):
+        loss_train = train_loop(env.dataloaders["train"], optimizer, criterion_train, model, env.args.clip)
+        if isinstance(optimizer, NTAvgSGD):
+            optimizer.set_average_weights()
+        ppl_dev, loss_dev = eval_loop(env.dataloaders["dev"], criterion_eval, model)
+        if isinstance(optimizer, NTAvgSGD):
+            optimizer.reset_weights()
+        if  ppl_dev < best_ppl:
+            best_ppl = ppl_dev
+            best_model = copy.deepcopy(model).to('cpu')
+            current_patience = 3
+        else:
+            current_patience -= 1
+        logger.add_epoch_log(epoch, np.asarray(loss_train).mean(), np.asarray(loss_dev).mean(), ppl_dev)
+        if current_patience <= 0:
+            break  
+    best_model.to(env.device)
+    if isinstance(optimizer, NTAvgSGD):
+        optimizer.set_average_weights()
     final_ppl, _ = eval_loop(env.dataloaders["test"], criterion_eval, best_model)
     logger.set_final_ppl(final_ppl)
     print(logger.dumps())
