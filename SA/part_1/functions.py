ea3a132f3166045d8ff355fc0c7b3c6cc1acd003
Dalvit <christian.dalvit@\unitn.it>
2024-11-28 10:04:56 +0100

    change metric computation

--- SA/part_1/functions.py
+++ SA/part_1/functions.py
@@ -1,9 +1,12 @@
 import torch 
+from sklearn.metrics import f1_score
+
+from utils import Lang
 
 def train_loop(model, dataloder, optimizer, criterion, device, clip):
     model.train()
     loss_array = []
-    for X, y, samples in dataloder:
+    for X, y, _ in dataloder:
         optimizer.zero_grad()
         slots = model(X)
         loss = criterion(slots.to(device), y)
@@ -13,7 +16,7 @@ def train_loop(model, dataloder, optimizer, criterion, device, clip):
         optimizer.step()
     return loss_array
 
-def eval_loop(model, dataloader, criterion, num_classes, device):
+def eval_loop(model, dataloader, criterion, lang: Lang, device):
     model.eval()
     loss_array = []
     ys = []
@@ -26,31 +29,13 @@ def eval_loop(model, dataloader, criterion, num_classes, device):
             ys.extend(y)
             slots_array.append(slots)
             
-            metric_inputs = []
+            gt_array = []
+            pred_array = []
             for idx, slot in enumerate(slots):
                 pred = slot.softmax(dim=0).argmax(dim=0)
                 slot_len = len(samples[idx]['tokens'])
-                metric_inputs.append((y[idx][:slot_len], pred[:slot_len]))
+                gt_array.append(y[idx][:slot_len].cpu())
+                pred_array.append(pred[:slot_len].cpu())
                 
-        return loss_array, evaluate(metric_inputs, num_classes)
-    
-def evaluate(metrics, num_classes, eps=1e-8):
-    tp, fp, fn = 0, 0, 0
-    for gt, pred in metrics:
-        for c in range(num_classes): 
-            tp += ((pred == c) & (gt == c)).sum().float()
-            fp += ((pred == c) & (gt != c)).sum().float()
-            fn += ((pred != c) & (gt == c)).sum().float()
-            
-    # Compute macro and micro averages
-    precision = tp / (tp + fp + eps)  # Add epsilon to avoid division by zero
-    recall = tp / (tp + fn + eps)
-    f1_score = 2 * (precision * recall) / (precision + recall + eps)
-    return {
-        "class_precision": precision.tolist(),
-        "class_recall": recall.tolist(),
-        "class_f1": f1_score.tolist(),
-        "macro_precision": precision.mean().item(),
-        "macro_recall": recall.mean().item(),
-        "macro_f1": f1_score.mean().item(),
-    }
+        f1s = f1_score(gt_array, pred_array, average=None)
+        return loss_array, {v: f1 for v, f1 in zip(lang.slot2id.keys(), f1s)}
